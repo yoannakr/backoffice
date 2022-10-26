63c57be60312eef23b2c7e8723e5a9712d0178b8
yoannakr <jkrmilkova@gmail.com>
2022-10-26 10:40:28 +0300

    Created file with task statuses.

---
+++ src/features/tasks/features/TasksFilter/taskStatuses.ts
@@ -0,0 +1,6 @@
+import { TaskStatusType } from "../../../../types/tasks";
+
+export const taskStatuses = [
+  { label: "Completed", value: TaskStatusType.COMPLETED },
+  { label: "Not completed", value: TaskStatusType.NOTCOMPLETED },
+];
